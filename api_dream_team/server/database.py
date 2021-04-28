5539631d7253076c66a0a7dd9b0bc8655122f3f4
renatohkuramoto <renato.kuramoto@olos.com.br>
2021-04-27 23:07:44 -0300

    Updated

--- api_dream_team/server/database.py
+++ api_dream_team/server/database.py
@@ -2,7 +2,7 @@ import motor.motor_asyncio
 from bson.objectid import ObjectId
 
 def get_collection(database, collection):
-    MONGO_URL = "mongodb+srv://admin:@cluster0.xfmcv.mongodb.net/{}?retryWrites=true&w=majority".format(database)
+    MONGO_URL = "mongodb+srv://admin:re200990@cluster0.xfmcv.mongodb.net/{}?retryWrites=true&w=majority".format(database)
     client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
     database = client[database]
     return database.get_collection("{}_collection".format(collection))
